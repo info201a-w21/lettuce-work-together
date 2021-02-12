8963f032225e3555c8fe8388e8d97231aa055fb6
Gabby-Alonso <alonsg2@uw.edu>
2021-02-11 22:21:49 -0800

    started isolating food insecurity data

--- scripts/Summary Information/Summary Information.R
+++ scripts/Summary Information/Summary Information.R
@@ -1,12 +1,15 @@
 # Summary Information Script 
 
-#Install and load packages 
+# Install and load packages 
 install.packages("readxl")
 library(tidyverse)
 library(dplyr)
 library(readxl)
 
-#Load state data
+install.packages("plyr")
+library(plyr)
+
+# Load state data
 fa_2009 <- read_excel("DATA/Feeding America Data/MMG2011_2009Data_ToShare.xlsx", sheet = "State")
 fa_2010 <- read_excel("DATA/Feeding America Data/MMG2012_2010Data_ToShare.xlsx", sheet = "State")
 fa_2011 <- read_excel("DATA/Feeding America Data/MMG2013_2011Data_ToShare.xlsx", sheet = "2011 State ")
@@ -18,4 +21,25 @@ fa_2016 <- read_excel("DATA/Feeding America Data/MMG2018_2016Data_ToShare.xlsx",
 fa_2017 <- read_excel("DATA/Feeding America Data/MMG2019_2017Data_ToShare.xlsx", sheet = "2017 State")
 fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx", skip =1, sheet = "2018 State")
 
-#add sheet name 
+#A table that unites yearly data on food insecurity 
+
+#Make a table of only food insecurity per year 
+
+food_insecurity_09 <-as.data.frame(fa_2009[,c("State Name", "Number Food Insecure Individuals")])
+food_insecurity_10 <-as.data.frame(fa_2010 [, c( "State", "Estimated Number of Food Insecure Persons in 2010")])
+
+both_years <-full_join(food_insecurity_09, food_insecurity_10, by =c("Number Food Insecure Individuals" = "Estimated Number of Food Insecure Persons in 2010", "State Name" = "State"))
+
+#I combined it but i does not add the numbers together, only gives me the x value 
+
+
+
+# A function that takes in the states data set from 2009 - 2018 and returns a
+# list of information about it: 
+
+summary_insecurity_rate <- list()
+summary_insecurity_rate$state_max_insecurity <-
+  
+  
+
+  #c(fa_2009, fa_2010, fa_2011, fa_2012, fa_2013, fa_2014, fa_2015, fa_2016, fa_2017, fa_2018))
