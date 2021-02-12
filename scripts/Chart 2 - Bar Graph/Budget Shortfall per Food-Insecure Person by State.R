c03780454ea458df31813d02f68652b0e3418748
Maynard Maynard-Zhang <>
2021-02-11 19:12:13 -0800

    Start fixing data loading

--- scripts/Chart 2 - Bar Graph/Budget Shortfall per Food-Insecure Person by State.R
+++ scripts/Chart 2 - Bar Graph/Budget Shortfall per Food-Insecure Person by State.R
@@ -7,7 +7,7 @@ library(ggplot2)
 library(readxl)
 
 # Load data
-fa_2009 <- read_excel("DATA/Feeding America Data/MMG2011_2009Data_ToShare.xlsx")
+fa_2009 <- read_excel("DATA/Feeding America Data/MMG2011_2009Data_ToShare.xlsx", sheet = "State")
 fa_2010 <- read_excel("DATA/Feeding America Data/MMG2012_2010Data_ToShare.xlsx")
 fa_2011 <- read_excel("DATA/Feeding America Data/MMG2013_2011Data_ToShare.xlsx")
 fa_2012 <- read_excel("DATA/Feeding America Data/MMG2014_2012Data_ToShare.xlsx")
@@ -16,7 +16,7 @@ fa_2014 <- read_excel("DATA/Feeding America Data/MMG2016_2014Data_ToShare.xlsx")
 fa_2015 <- read_excel("DATA/Feeding America Data/MMG2017_2015Data_ToShare.xlsx")
 fa_2016 <- read_excel("DATA/Feeding America Data/MMG2018_2016Data_ToShare.xlsx")
 fa_2017 <- read_excel("DATA/Feeding America Data/MMG2019_2017Data_ToShare.xlsx")
-fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx")
+fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx", skip = 1)
 
 # Assemble variables
 # States
