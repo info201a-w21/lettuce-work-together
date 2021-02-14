c1b04f62d5ed93d4be3adb086523245142655af2
Gabby-Alonso <alonsg2@uw.edu>
2021-02-13 21:55:15 -0800

    summary about food insecurity in 2018

--- scripts/Summary Information/Summary Information.R
+++ scripts/Summary Information/Summary Information.R
@@ -1,45 +1,51 @@
-# Summary Information Script 
+# GABRIELLA ALONSO
+# Summary Information  
 
 # Install and load packages 
 install.packages("readxl")
+install.packages("plyr")
 library(tidyverse)
 library(dplyr)
 library(readxl)
-
-install.packages("plyr")
 library(plyr)
 
-# Load state data
-fa_2009 <- read_excel("DATA/Feeding America Data/MMG2011_2009Data_ToShare.xlsx", sheet = "State")
-fa_2010 <- read_excel("DATA/Feeding America Data/MMG2012_2010Data_ToShare.xlsx", sheet = "State")
-fa_2011 <- read_excel("DATA/Feeding America Data/MMG2013_2011Data_ToShare.xlsx", sheet = "2011 State ")
-fa_2012 <- read_excel("DATA/Feeding America Data/MMG2014_2012Data_ToShare.xlsx", sheet = "2012 State")
-fa_2013 <- read_excel("DATA/Feeding America Data/MMG2015_2013Data_ToShare.xlsx", sheet = "2013 State")
-fa_2014 <- read_excel("DATA/Feeding America Data/MMG2016_2014Data_ToShare.xlsx", sheet = "2014 State")
-fa_2015 <- read_excel("DATA/Feeding America Data/MMG2017_2015Data_ToShare.xlsx", sheet = "2015 State")
-fa_2016 <- read_excel("DATA/Feeding America Data/MMG2018_2016Data_ToShare.xlsx", sheet = "2016 State")
-fa_2017 <- read_excel("DATA/Feeding America Data/MMG2019_2017Data_ToShare.xlsx", sheet = "2017 State")
-fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx", skip =1, sheet = "2018 State")
+# Load data
 
-#A table that unites yearly data on food insecurity 
+fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx", 
+  skip =1, sheet = "2018 State"
+  )
 
-#Make a table of only food insecurity per year 
+#Function that lists summary information about 2018 Food insecurity  
 
-food_insecurity_09 <-as.data.frame(fa_2009[,c("State Name", "Number Food Insecure Individuals")])
-food_insecurity_10 <-as.data.frame(fa_2010 [, c( "State", "Estimated Number of Food Insecure Persons in 2010")])
-
-both_years <-full_join(food_insecurity_09, food_insecurity_10, by =c("Number Food Insecure Individuals" = "Estimated Number of Food Insecure Persons in 2010", "State Name" = "State"))
-
-#I combined it but i does not add the numbers together, only gives me the x value 
-
-
-
-# A function that takes in the states data set from 2009 - 2018 and returns a
-# list of information about it: 
-
-summary_insecurity_rate <- list()
-summary_insecurity_rate$state_max_insecurity <-
+summary__fd_insecurity_2018<- list(most_food_insecure, least_food_insecure, 
+                                avg_people_insecure)
   
+# Pull the state with the highest food insecurity rate in 2018
+  state_max_insecurity <- fa_2018 %>% 
+    select(`State Name`, `2018 Food Insecurity Rate`) %>% 
+  filter(`2018 Food Insecurity Rate` == max(`2018 Food Insecurity Rate`, na.rm =T)) %>% 
+    pull(`State Name`) 
+ #Mississippi  
   
+# Function that explains state max insecurity information for summary 
+  most_food_insecure <- paste(state_max_insecurity, "was the most food insecure state of 2018")
+    
+#Pull the state with the lowest food insecurity rate in 2018
+state_min_insecurity <- fa_2018 %>% 
+  select (`State Name`,`2018 Food Insecurity Rate`) %>% 
+  filter(`2018 Food Insecurity Rate` == min(`2018 Food Insecurity Rate`, na.rm =T)) %>%
+  pull(`State Name`) 
+#North Dakota 
 
-  #c(fa_2009, fa_2010, fa_2011, fa_2012, fa_2013, fa_2014, fa_2015, fa_2016, fa_2017, fa_2018))
+#Function that explains state min insecurity information for summary 
+
+least_food_insecure <-paste(state_min_insecurity, "was the least food insecure state of 2018")
+
+#Pull average number of people who are food insecure in the US in 2018 
+#The data included WA DC as a state, which is why I divided by 51 
+Avg_num_food_insecure <- (sum(fa_2018$`# of Food Insecure Persons in 2018`))/51 %>% 
+# about 782,796 food insecure people per state
+
+#Function that explains the average number of food insecure people in 2018 
+avg_people_insecure <-paste("In 2018, there was an average of", round(Avg_num_food_insecure), "people who were food insecure 
+                            in the United States")
