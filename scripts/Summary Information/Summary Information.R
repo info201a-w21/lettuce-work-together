1b2c1516d26adb11d11106534abef8c894362ec4
Gabby-Alonso <alonsg2@uw.edu>
2021-02-13 22:35:36 -0800

    Added functions about meal costs

--- scripts/Summary Information/Summary Information.R
+++ scripts/Summary Information/Summary Information.R
@@ -15,7 +15,7 @@ fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx",
   skip =1, sheet = "2018 State"
   )
 
-#Function that lists summary information about 2018 Food insecurity  
+#Function that lists 2018 summary information about Food insecurity  
 
 summary__fd_insecurity_2018<- list(most_food_insecure, least_food_insecure, 
                                 avg_people_insecure)
@@ -25,7 +25,7 @@ summary__fd_insecurity_2018<- list(most_food_insecure, least_food_insecure,
     select(`State Name`, `2018 Food Insecurity Rate`) %>% 
   filter(`2018 Food Insecurity Rate` == max(`2018 Food Insecurity Rate`, na.rm =T)) %>% 
     pull(`State Name`) 
- #Mississippi  
+ # Mississippi  
   
 # Function that explains state max insecurity information for summary 
   most_food_insecure <- paste(state_max_insecurity, "was the most food insecure state of 2018")
@@ -35,16 +35,67 @@ state_min_insecurity <- fa_2018 %>%
   select (`State Name`,`2018 Food Insecurity Rate`) %>% 
   filter(`2018 Food Insecurity Rate` == min(`2018 Food Insecurity Rate`, na.rm =T)) %>%
   pull(`State Name`) 
-#North Dakota 
+# North Dakota 
 
-#Function that explains state min insecurity information for summary 
+# Function that explains state min insecurity information for summary 
 least_food_insecure <-paste(state_min_insecurity, "was the least food insecure state of 2018")
 
-#Pull average number of people who are food insecure in the US in 2018 
+# Pull average number of people who are food insecure in the US in 2018 
 #The data included WA DC as a state, which is why I divided by 51 
 Avg_num_food_insecure <- (sum(fa_2018$`# of Food Insecure Persons in 2018`))/51 %>% 
 # about 782,796 food insecure people per state
 
-#Function that explains the average number of food insecure people in 2018 
+# Function that explains the average number of food insecure people in 2018 
 avg_people_insecure <-paste("In 2018, there was an average of", round(Avg_num_food_insecure), 
                             "people per state who were food insecure in the United States")
+
+
+
+
+
+# Function that lists 2018 summary information about Meal Costs 
+summary_meal_costs_2018 <- list(summary_expensive_meal_2018,summary_cheapest_meal_2018,summary_avg_cost)
+
+# Pull the state with the highest meal cost 
+most_expensive_meal <- fa_2018 %>% 
+  select(`State Name`, `2018 Cost Per Meal`) %>% 
+  filter(`2018 Cost Per Meal`==max(`2018 Cost Per Meal`)) %>% 
+  pull(`State Name`) 
+# Washington, D.C. 
+
+# Pull price of the highest meal 
+price_most_expensive_meal <-fa_2018 %>% 
+  select(`State Name`, `2018 Cost Per Meal`) %>% 
+  filter(`2018 Cost Per Meal`==max(`2018 Cost Per Meal`)) %>% 
+  pull(`2018 Cost Per Meal`) 
+# $4.08
+
+# Function that informs about the most expensive meal 
+summary_expensive_meal_2018 <-paste(most_expensive_meal, 
+      "had the most expensive average meal cost of 2018:$", price_most_expensive_meal)
+
+# Pull the state with the lowest meal cost 
+cheapest_meal_2018 <-fa_2018 %>% 
+  select(`State Name`, `2018 Cost Per Meal`) %>% 
+  filter(`2018 Cost Per Meal`==min(`2018 Cost Per Meal`)) %>% 
+  pull(`State Name`) 
+# Texas 
+
+#Pull price of the lowest meal 
+price_cheapest_meal <-fa_2018 %>% 
+  select(`State Name`, `2018 Cost Per Meal`) %>% 
+  filter(`2018 Cost Per Meal`==min(`2018 Cost Per Meal`)) %>% 
+  pull(`2018 Cost Per Meal`) 
+# $2.64
+
+#Function that informs about the cheapest meal 
+summary_cheapest_meal_2018 <-paste(cheapest_meal_2018, 
+     "had the lowest average meal cost of 2018: $", price_cheapest_meal)
+
+#Function that calculates average meal cost in US
+#The data included WA DC as a state, which is why I divided by 51 
+Avg_meal_cost <- (sum(fa_2018$`2018 Cost Per Meal`))/51
+# 3.089411764
+
+#Function that explains the average meal cost 
+summary_avg_cost <-paste("In 2018, the average meal cost was $", round(Avg_meal_cost))
