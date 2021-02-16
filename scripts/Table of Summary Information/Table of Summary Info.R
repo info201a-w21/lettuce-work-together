62ad0d84ffcb10a00c47e1d9e8c04539d2db6acd
Gabby-Alonso <alonsg2@uw.edu>
2021-02-15 17:56:46 -0800

    Create Aggregate table

--- scripts/Table of Summary Information/Table of Summary Info.R
+++ scripts/Table of Summary Information/Table of Summary Info.R
@@ -1,3 +1,28 @@
 # GABRIELLA ALONSO
+# Table of Summary Information
 
-#Table of Summary Information
+
+# Install and load packages 
+install.packages("readxl")
+install.packages("plyr")
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(plyr)
+
+# Load data 
+fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx", 
+                      skip =1, sheet = "2018 State"
+)
+
+
+# Aggregate table focused on the financial aspects of food insecurity
+
+financial_df <- fa_2018 %>% 
+select(`State Name`,`2018 Cost Per Meal`, `2018 Weighted Annual Food Budget Shortfall`)  %>% 
+ group_by(`State Name`) %>% 
+  mutate(`Meals Not Consumed due to Budget Shortfall` =`2018 Weighted Annual Food Budget Shortfall`/ `2018 Cost Per Meal`) 
+
+
+  
+  
