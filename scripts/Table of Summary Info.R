053b7ec604fd08bda6a507d492f8f7dc45bc372d
Gabby-Alonso <alonsg2@uw.edu>
2021-02-17 17:58:01 -0800

    Added commas to my table (yay)

--- scripts/Table of Summary Info.R
+++ scripts/Table of Summary Info.R
@@ -5,11 +5,12 @@
 # Install and load packages 
 install.packages("readxl")
 install.packages("plyr")
+install.packages("scales")
 library(tidyverse)
 library(dplyr)
 library(readxl)
 library(plyr)
-
+library(scales)
 # Load data 
 fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx", 
                       skip =1, sheet = "2018 State"
@@ -20,7 +21,10 @@ fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx",
 financial_df <- fa_2018 %>% 
 select(`State Name`,`2018 Cost Per Meal`, `2018 Weighted Annual Food Budget Shortfall`)  %>% 
  group_by(`State Name`) %>% 
-  mutate(`Meals Not Consumed due to Budget Shortfall` =`2018 Weighted Annual Food Budget Shortfall`/ `2018 Cost Per Meal`) 
+  mutate(`Meals Not Consumed due to Budget Shortfall` =`2018 Weighted Annual Food Budget Shortfall`/ `2018 Cost Per Meal`) %>% 
+mutate(`Meals Not Consumed due to Budget Shortfall`= number(`Meals Not Consumed due to Budget Shortfall`, big.mark= ",")) %>% 
+mutate(`2018 Weighted Annual Food Budget Shortfall` = number(`2018 Weighted Annual Food Budget Shortfall`, big.mark = ","))
+
 
 # Pertient Observations
 max_meals_lost <-financial_df %>% 
