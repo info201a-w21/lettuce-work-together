af5e3c4268eeab483956462be20d6aaa7a2f170e
Maynard Maynard-Zhang <>
2021-02-18 15:36:29 -0800

    Write analysis for Chart 2

--- scripts/Chart 2 - Shortfall per Insecure 2018.R
+++ scripts/Chart 2 - Shortfall per Insecure 2018.R
@@ -7,7 +7,6 @@ library(ggplot2)
 library(readxl)
 library(styler)
 library(lintr)
-library(RColorBrewer)
 
 # Load data
 fa_2018 <- read_excel("DATA/Feeding America Data/MMG2020_2018Data_ToShare.xlsx",
@@ -24,22 +23,33 @@ fa_2018 <- fa_2018 %>%
   mutate(shortfall_per_insecure = ((`2018 Weighted Annual Food Budget Shortfall`
   / `# of Food Insecure Persons in 2018`) %>% round(digits = 2)))
 
-# Top 10 shortfalls
-fa_2018_top_10_shortfall <- fa_2018 %>%
+# Top 5 shortfalls
+shortfall_top_5 <- fa_2018 %>%
   select(`State Name`, shortfall_per_insecure) %>%
-  top_n(10, shortfall_per_insecure) %>%
-  arrange(desc(shortfall_per_insecure))
+  arrange(desc(shortfall_per_insecure)) %>%
+  top_n(5, shortfall_per_insecure)
+
+# Bottom 5 shortfalls
+shortfall_bottom_5 <- fa_2018 %>%
+  select(`State Name`, shortfall_per_insecure) %>%
+  arrange(desc(shortfall_per_insecure)) %>%
+  top_n(-5, shortfall_per_insecure)
+
+# Join top and bottom 5
+shortfalls <- full_join(shortfall_top_5, shortfall_bottom_5,
+  by = c("State Name", "shortfall_per_insecure")
+)
 
 # Create visualization - bar graph
 # 1 categorical (state) and 1 continuous (shortfall) variable
-shortfall_per_insecure <- ggplot(data = fa_2018_top_10_shortfall) +
+chart2 <- ggplot(data = shortfalls) +
   geom_col(mapping = aes(
     x = reorder(`State Name`, shortfall_per_insecure),
     y = shortfall_per_insecure,
     fill = `State Name`
   )) +
   labs(
-    title = "Budget Shortfall per Food-Insecure Person",
+    title = "Top 5 and Bottom 5 State Shortfalls",
     x = "State",
     y = "Shortfall ($)"
   ) +
