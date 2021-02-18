108bc799a9464e61acb28ea95c58a5bdeddf08f6
Gabby-Alonso <alonsg2@uw.edu>
2021-02-17 23:20:15 -0800

    Added table analysis

--- scripts/Table of Summary Info.R
+++ scripts/Table of Summary Info.R
@@ -25,9 +25,22 @@ select(`State Name`,`2018 Cost Per Meal`, `2018 Weighted Annual Food Budget Shor
 mutate(`Meals Not Consumed due to Budget Shortfall`= number(`Meals Not Consumed due to Budget Shortfall`, big.mark= ",")) %>% 
 mutate(`2018 Weighted Annual Food Budget Shortfall` = number(`2018 Weighted Annual Food Budget Shortfall`, big.mark = ","))
 
+# Pertinent Observations 
 
-# Pertient Observations
-max_meals_lost <-financial_df %>% 
-  select(`State Name`, `Meals Not Consumed due to Budget Shortfall`)%>% 
-  filter(`Meals Not Consumed due to Budget Shortfall`==max(`Meals Not Consumed due to Budget Shortfall`, na.rm =T))%>% 
-  pull(`State Name`) 
+# Find state with the most meal loss 
+max_meal_lost <-financial_df %>% 
+  select(`State Name`,`Meals Not Consumed due to Budget Shortfall`) %>% 
+  group_by(`Meals Not Consumed due to Budget Shortfall`) %>% 
+  filter(`Meals Not Consumed due to Budget Shortfall`==max(`Meals Not Consumed due to Budget Shortfall`)) %>% 
+  pull(`State Name`)
+
+# Even though Texas as the cheapest meal cost, it has the highest meal loss due to budget shortfalls 
+
+# Find State with least meal loss
+min_meal_lost <-financial_df %>% 
+  select(`State Name`,`Meals Not Consumed due to Budget Shortfall`) %>% 
+  group_by(`Meals Not Consumed due to Budget Shortfall`) %>% 
+  filter(`Meals Not Consumed due to Budget Shortfall`==min(`Meals Not Consumed due to Budget Shortfall`)) %>% 
+  pull(`State Name`)
+
+# North Dakota continues has both the cheapest meal cost and lowest meal loss from budget shortfalls 
