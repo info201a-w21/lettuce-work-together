cb61bdec6e22790c364f0772004b665deb7cc2f5
Brielle Rose Bush <briellerosebush@Brielles-MacBook-Air.local>
2021-03-14 16:49:48 -0700

    styled

--- app.R
+++ app.R
@@ -1,12 +1,6 @@
-# Load libraries
 library("shiny")
 
-# Load data (later step)
-
-# Use source() to execute the `app_ui.R` and `app_server.R` files. These will
-# define the UI value and server function respectively.
 source("app_server.R")
 source("app_ui.R")
 
-# Create a new `shinyApp()` using the loaded `ui` and `server` variables
 shinyApp(ui = ui, server = server)
